5da0e880be0956d8d243de7bfbb2343de7bdd5f2
Aneesh Bharadwaj <aneesh.bharadwaj7@gmail.com>
2017-01-19 16:46:08 +0530

    Final

---
+++ functionArguments.js
@@ -0,0 +1,7 @@
+function ArithmeticOperation(firstArg,secondArg,thirdArg){
+    var multiplyTwoNumbers = secondArg * thirdArg ;
+    var result = firstArg + multiplyTwoNumbers ;
+    return result;
+    
+}
+console.log(ArithmeticOperation(53, 61, 67));
---
+++ functions.js
@@ -0,0 +1,5 @@
+function eat(foodItem){
+    return (foodItem + ' tasted really good.');
+    
+}
+console.log(eat('bananas'));
